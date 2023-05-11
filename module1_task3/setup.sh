afec0f6dac57c5b23c6cbbe8af284b5cfe6f2141
Aminekham <lkham0508@gmail.com>
2023-05-11 14:55:18 +0200

    fixing

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -9,14 +9,7 @@ apt-get install make
 
 hugo new site module1_task3
 cd module1_task3
-git submodule add --force https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
 
 make build
 
-if [ $? -ne 0 ]; then
-    echo "Build failed. Please check the error message above."
-    exit 1
-fi
-
-# Exit successfully if the build succeeded
 exit 0
